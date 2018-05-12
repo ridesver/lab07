9b4cc7899d6d95910c4ddb0721c9027bcaf4f3f2
ridesver <ridesver@gmail.com>
2018-05-12 15:34:00 +0300

    added documentation

--- README.md
+++
@@ -1,1 +0,0 @@
-[![Build Status](https://travis-ci.org/ridesver/lab07.svg?branch=master)](https://travis-ci.org/ridesver/lab07)
--- include/print.hpp
+++
@@ -1,6 +0,0 @@
-#include <string>
-#include <fstream>
-#include <iostream>
-
-void print(const std::string& text, std::ostream& out = std::cout); //функция выводит строку в поток 
-void print(const std::string& text, std::ofstream& out); //функция выводит строку в файл
